6ab1ac8058dc80c8e481e6bf81515fd168b8ad2b
bingerjiang <binger.jiang@mail.mcgill.ca>
2022-05-10 08:39:49 +0200

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
-export CUDA_VISIBLE_DEVICES=7
-python turngpt/train.py   --gpus 1 \
+
+python turngpt/train_conditional.py   --gpus -1 \
                         --batch_size 2 \
-                        --accumulate_grad_batches 10
+                        --accumulate_grad_batches 10\
+                        --trp_projection_steps 1\
+                        --strategy=ddp
 
 # export CUDA_VISIBLE_DEVICES=7
 # python turngpt/model.py
+
+# export CUDA_VISIBLE_DEVICES=5
+# python turngpt/train.py   --gpus 1 \
+#                         --batch_size 4 \
+#                         --accumulate_grad_batches 10\
+#                         --trp_projection_steps 1
